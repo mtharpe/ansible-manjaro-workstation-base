3b0968ca5a448bedcd159454d5d7e2ea3c3b335d
Michael Tharpe <mtharpe@users.noreply.github.com>
2021-09-10 02:09:22 -0400

    Update run and Vagrant

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ if [ ! -f /bin/ansible ]; then
     sudo pacman -Syy --noconfirm ansible 
 fi
 
+if [ ! -f /etc/sudoers.d/tharpem ]; then
+    echo "tharpem ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/tharpem 
+fi
+
 ansible-playbook --extra-vars "local_user=${USER}" site.yml
 
 sudo -u gdm dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
