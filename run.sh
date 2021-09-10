93982a3dd64a932bf3841c909280140e0de040e0
Michael Tharpe <mtharpe@users.noreply.github.com>
2021-09-10 15:40:50 -0400

    Updates to script and removal of junk files

--- run.sh
+++ run.sh
@@ -10,7 +10,9 @@ if [ ! -f /etc/sudoers.d/tharpem ]; then
     echo "tharpem ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/tharpem 
 fi
 
-ansible-playbook --extra-vars "local_user=${USER}" site.yml
+until ansible-playbook --extra-vars "local_user=${USER}" site.yml; do
+  echo Ansible run disrupted, retrying in 10 seconds...
+  sleep 10
+done
 
 sudo -u gdm dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
-
